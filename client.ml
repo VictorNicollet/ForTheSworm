9efab81151098bfa8dbef5105c9f37f433b5265e
Victor Nicollet <victor@nicollet.net>
2012-12-02 09:07:33 +0100

    Test sending data for saving

--- client.ml
+++ client.ml
@@ -5,9 +5,17 @@ let connect ~port =
   Unix.(connect sock (ADDR_INET (inet_addr_loopback,port))) ;
   let read = new SocketStream.read sock in
   let write = new SocketStream.write sock in
+
+  (* Shake hands *)
   Protocol.Handshake.send ~version:Protocol.version write ;
   let version = Protocol.Handshake.recv read in
-  Printf.printf "Version : %d\n" version ; 
+  Printf.printf "Version: %d\n" version ; 
+
+  (* Upload some data *)
+  Protocol.Save.send ~data:"Hello, world!" write ;
+  let id = Protocol.Save.recv read in
+  Printf.printf "Saved: %s\n" (Key.to_hex_short id) ;
+
   Unix.(shutdown sock SHUTDOWN_ALL) 
 
 let () = connect ~port:4567
--- server.ml
+++ server.ml
@@ -8,6 +8,7 @@ end
 
 let () = Listener.start ~port:4567 ~max:10 (fun _ read write -> 
   let rec loop () = 
+    Log.(out DEBUG "Pulling next request...") ; 
     let request = Protocol.parseNextRequest read in
     request server write ;
     loop ()
