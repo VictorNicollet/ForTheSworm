cef3972b8d631f58f808b30a48453fd0646757c6
Victor Nicollet <victor@nicollet.net>
2012-12-02 09:14:36 +0100

    Display client-server operation statistics

--- server.ml
+++ server.ml
@@ -8,10 +8,12 @@ end
 
 let () = Listener.start ~port:4567 ~max:10 (fun _ read write -> 
   let rec loop () = 
-    Log.(out DEBUG "Pulling next request...") ; 
     let request = Protocol.parseNextRequest read in
     request server write ;
     loop ()
   in
-  try loop () with SocketStream.EOT -> () 
+  try loop () with SocketStream.EOT -> 
+    Log.(out AUDIT "Sent : %.3f KB, Received : %.3f KB"
+      (float_of_int (write # count) /. 1024.) 
+      (float_of_int (read  # count) /. 1024.)) 
 ) 
